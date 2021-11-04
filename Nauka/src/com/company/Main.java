390045d44efd8d0a97af6a3af1dc7a719811aa3a
PatrykBak <bakevan@gmail.com>
2021-11-04 17:49:09 +0100

    Initial commit

---
+++ Nauka/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+    }
+}
