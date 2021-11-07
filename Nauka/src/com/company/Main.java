863ce04d05ce62ca91ff2997603480a5fef78512
PatrykBak <bakevan@gmail.com>
2021-11-07 15:18:47 +0100

    Co-authored-by: Piotr-Skiba <Piotr-Skiba@users.noreply.github.com>

--- Nauka/src/com/company/Main.java
+++ Nauka/src/com/company/Main.java
@@ -7,6 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Bieda world !");
+        System.out.println("Hello Bieda world  !");
+        System.out.println(scanner.nextLine());
+        getKał("Enej");
+    }
+    static String getKał(String kał) {
+         return kał;
     }
 }
