549f7d75610444c2f2761329f028a0bbf9fcccc2
PatrykBak <bakevan@gmail.com>
2021-11-07 15:40:30 +0100

    enejusz

--- Nauka/src/com/company/Main.java
+++ Nauka/src/com/company/Main.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Scanner;
 
 public class Main {
@@ -8,10 +12,24 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello Bieda world  !");
-        System.out.println(scanner.nextLine());
-        getKał("Enej");
-    }
-    static String getKał(String kał) {
-         return kał;
+        try {
+            String name = scanner.nextLine();
+            URL url = new URL("https://api.agify.io/?name=" + name);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Accept", "application/json");
+            con.setDoOutput(true);
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                System.out.println(response.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
